d28590191fd842955bececed63a4d63f9b992691
Svetlana Voskresenskaya <svoskresenskaya@diasoft.ru>
2021-10-27 22:37:00 +0300

    init gateway repo less_13

---
+++ src/main/java/ru/diasoft/micro/GatewayServiceApplication.java
@@ -0,0 +1,14 @@
+package ru.diasoft.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/diasoft/micro/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.diasoft.micro;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
